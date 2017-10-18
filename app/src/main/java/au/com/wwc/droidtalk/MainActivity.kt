f00d3a8003da5d1fd85cf538cfc126f95f223b55
Jessie Leung <jleu1656@gmail.com>
2017-10-18 15:13:45 +1100

    Base code for kotlin droidtalk app

---
+++ app/src/main/java/au/com/wwc/droidtalk/MainActivity.kt
@@ -0,0 +1,12 @@
+package au.com.wwc.droidtalk
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
