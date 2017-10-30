695e6017dc4cbe89f840ef8cd883ba91cc76c237
Jessie Leung <jleu1656@gmail.com>
2017-10-30 20:00:42 +1100

    Show static list of messages

--- app/src/main/java/au/com/wwc/droidtalk/ui/MainActivity.kt
+++ app/src/main/java/au/com/wwc/droidtalk/ui/MainActivity.kt
@@ -1,7 +1,9 @@
 package au.com.wwc.droidtalk.ui
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import au.com.wwc.droidtalk.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +11,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val listView = findViewById(R.id.messages_list) as ListView
+        val messages = ArrayList<String>()
+        messages.add("Message 1")
+        messages.add("Message 2")
+        messages.add("Message 3")
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, messages)
+        listView.adapter = adapter
     }
 }
