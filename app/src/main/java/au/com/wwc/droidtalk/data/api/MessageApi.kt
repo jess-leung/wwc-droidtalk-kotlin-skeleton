a9ff0271b88b0b24f12f513977a3f52f83f28d67
Jessie Leung <jleu1656@gmail.com>
2017-10-30 21:03:16 +1100

    Add internet permissions; change to use message api to send and get messages

--- app/src/main/java/au/com/wwc/droidtalk/data/api/MessageApi.kt
+++ app/src/main/java/au/com/wwc/droidtalk/data/api/MessageApi.kt
@@ -1,7 +1,31 @@
 package au.com.wwc.droidtalk.data.api
 
+import au.com.wwc.droidtalk.data.model.Message
+import io.reactivex.Observable
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
 interface MessageApi {
 
-    // RETROFIT ENDPOINTS HERE
+    @GET("/messages")
+    fun getMessages(): Observable<List<Message>>
 
+    @POST("/messages")
+    fun sendMessage(@Body message: Message): Observable<Message>
+
+    companion object Factory {
+        fun create(): MessageApi {
+            val retrofit = Retrofit.Builder()
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .baseUrl("http://wwc-chatroom-staging.herokuapp.com/")
+                    .build()
+
+            return retrofit.create(MessageApi::class.java);
+        }
+    }
 }
--- app/src/main/java/au/com/wwc/droidtalk/ui/MainActivity.kt
+++ app/src/main/java/au/com/wwc/droidtalk/ui/MainActivity.kt
@@ -7,25 +7,59 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ListView
 import au.com.wwc.droidtalk.R
+import au.com.wwc.droidtalk.data.api.MessageApi
+import au.com.wwc.droidtalk.data.model.Message
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var messages: ArrayList<String>
+    private lateinit var adapter: ArrayAdapter<String>
+    private lateinit var messageApi: MessageApi
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val listView = findViewById(R.id.message_list) as ListView
-        val messages = ArrayList<String>()
-        messages.add("Message 1")
-        messages.add("Message 2")
-        messages.add("Message 3")
-        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, messages)
+        messages = ArrayList()
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, messages)
         listView.adapter = adapter
 
+        messageApi = MessageApi.create()
+
+        setUpMessagesList()
+
+        setUpSendingMessages()
+    }
+
+    private fun setUpMessagesList() {
+        messageApi.getMessages()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe({ retrievedMessages ->
+                    for (message in retrievedMessages) {
+                        messages.add(message.content)
+                    }
+                    adapter.notifyDataSetChanged()
+                }, { error ->
+                    error.printStackTrace()
+                })
+    }
+
+    private fun setUpSendingMessages() {
         val submitButton = findViewById(R.id.message_button) as Button
         val messageEditText = findViewById(R.id.message_input) as EditText
         submitButton.setOnClickListener {
-            messages.add(messageEditText.text.toString())
-            adapter.notifyDataSetChanged()
+            messageApi.sendMessage(Message(messageEditText.text.toString()))
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribeOn(Schedulers.io())
+                    .subscribe({ _ ->
+                        setUpMessagesList()
+                    }, { error ->
+                        error.printStackTrace()
+                    })
         }
     }
+
 }
